a6b02a43164b6a913e0009bf520e3e458c160cd8
sarathkella <sarathchandra231@gmail.com>
2018-06-11 14:32:00 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,81 +6,125 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  la: boolean = false;
+  c: Selection;
+  b: any;
+  @ViewChild('ev') ev: ElementRef
   title1: any;
   title = 'app';
-  data = "string";
+  data :any;
   myval: any;
+  a;
 
 boldtoggle:boolean = false;
 italictoggle: boolean = false;
 underlinetoggle: boolean = false;
+codetoggle: boolean = false;
+getit:boolean=false;
+ coloritems = ['red','blue','yellow','brown','skyblue','green','lightgreen']
+ sizeitems = ['small','medium','large']
 constructor(){
   this.myval = this.data;
   
 }
-datachange(event){
+datachange(event){ 
+   console.log(this.a)
   this.myval = event;
 } 
 boldfont(){
-  let editor = document.getElementById('textEditor');
-  const input : HTMLInputElement = <HTMLInputElement>editor
-  let start = input.selectionStart;
-  let end = input.selectionEnd;
-   let a = window.getSelection();
-   console.log('a ', a.toString())
+  this.c = window.getSelection();
+   this.a = this.c.toString();
    this.boldtoggle = !this.boldtoggle;
    if(this.boldtoggle){
-     var newreg = new RegExp(`${a}`,'gmi')
-     this.myval= this.myval.toString().replace(newreg,`<strong>${a}</strong>`);
-    //  this.boldtoggle = !this.boldtoggle;
+     this.b = this.a.bold()
+     var newreg = new RegExp(`${this.a}`,'gmi')
+     this.myval= this.myval.toString().replace(newreg,this.b);
+   
    }else{
     console.log(this.myval)
-    var newreg1 = new RegExp(`(<strong>)+${a}(</strong>)+`,'gmi')
-    console.log(newreg1)
-     this.myval = this.myval.toString().replace(newreg1,`${a}`);
-    console.log(this.myval)
+    var newreg1 = new RegExp(`(<b>)+(<.*>)*${this.a}(<\\/.*>)*(<\\/b>)+`,'gmi')
+    // console.log(newreg1)
+     this.myval = this.myval.toString().replace(newreg1,`${this.a}`);
+    // console.log(this.myval)
    }
 }
 itlalicfont(){
-  let editor = document.getElementById('textEditor');
-  const input : HTMLInputElement = <HTMLInputElement>editor
-  let start = input.selectionStart;
-  let end = input.selectionEnd;
-   let a = window.getSelection();
-   console.log('a ', a.toString())
-    // a.toString().replace(/(\\|s|\*)/gm,'');
+  this.c = window.getSelection();
+   this.a = this.c.toString();
   this.italictoggle = !this.italictoggle;
   if(this.italictoggle){
-    var newreg = new RegExp(`${a}`,'gmi')
-    this.myval= this.myval.toString().replace(newreg,`<i>${a}</i>`);
-   //  this.boldtoggle = !this.boldtoggle;
+    this.b= this.a.italics()
+    var newreg = new RegExp(`${this.a}`,'gmi')
+    this.myval= this.myval.toString().replace(newreg,this.b);
+   
   }else{
    console.log(this.myval)
-   var newreg1 = new RegExp(`(<i>)+${a}(</i>)+`,'gmi')
-   console.log(newreg1)
-    this.myval = this.myval.toString().replace(newreg1,`${a}`);
-   console.log(this.myval)
+   var newreg1 = new RegExp(`(<i>)+(<.*>)*${this.a}(<\\/.*>)*(<\\/i>)+`,'gmi')
+  //  console.log(newreg1)
+    this.myval = this.myval.toString().replace(newreg1,`${this.a}`);
+  //  console.log(this.myval)
   }
 }
 underlinefont(){
-  let editor = document.getElementById('textEditor');
-  const input : HTMLInputElement = <HTMLInputElement>editor
-  let start = input.selectionStart;
-  let end = input.selectionEnd;
-   let a = window.getSelection();
-   console.log('a ', a.toString())
+  this.c = window.getSelection();
+   this.a = this.c.toString();
   this.underlinetoggle = !this.underlinetoggle;
   if(this.underlinetoggle){
-    var newreg = new RegExp(`${a}`,'gmi')
-    this.myval= this.myval.toString().replace(newreg,`<u>${a}</u>`);
+    var newreg = new RegExp(`${this.a}`,'gmi')
+    this.myval= this.myval.toString().replace(newreg,`<u>${this.a}</u>`);
    //  this.boldtoggle = !this.boldtoggle;
   }else{
    console.log(this.myval)
-   var newreg1 = new RegExp(`(<u>)+${a}(</u>)+`,'gmi')
-   console.log(newreg1)
-    this.myval = this.myval.toString().replace(newreg1,`${a}`);
-   console.log(this.myval)
+   var newreg1 = new RegExp(`(<u>)+(<.*>)*${this.a}(<\\/.*>)*(<\\/u>)+`,'gmi')
+  //  console.log(newreg1)
+    this.myval = this.myval.toString().replace(newreg1,`${this.a}`);
+  //  console.log(this.myval)
   }
 }
 
+getthecode(event){
+  this.la= !this.la
+}
+
+gettheurl(urllink){
+  this.c = window.getSelection();
+  this.a = this.c.toString();
+this.b =this.a.link(urllink)
+    var newreg = new RegExp(`${this.a}`,'gmi')
+    this.myval= this.myval.toString().replace(newreg,this.b);
+}
+
+setcolor(colorvalue){
+  this.c = window.getSelection();
+   this.a = this.c.toString();
+this.b =this.a.fontcolor(colorvalue)
+     var newreg = new RegExp(`${this.a}`,'gmi')
+     this.myval= this.myval.toString().replace(newreg,this.b);
+}
+setfont(sizes){
+  this.c = window.getSelection();
+   this.a = this.c.toString();
+   this.boldtoggle = !this.boldtoggle;
+   if(this.boldtoggle){
+     if(sizes =='small'){
+      this.b = this.a.small()
+     }
+     if(sizes =='medium'){
+      this.b = this.a.bold()
+     }
+     if(sizes =='large'){
+      this.b = this.a.big()
+     }
+     
+     var newreg = new RegExp(`${this.a}`,'gmi')
+     this.myval= this.myval.toString().replace(newreg,this.b);
+   
+   }else{
+    console.log(this.myval)
+    var newreg1 = new RegExp(`(<b>)+(<.*>)*${this.a}(<\\/.*>)*(<\\/b>)+`,'gmi')
+    // console.log(newreg1)
+     this.myval = this.myval.toString().replace(newreg1,`${this.a}`);
+    // console.log(this.myval)
+   }
+}
 }
